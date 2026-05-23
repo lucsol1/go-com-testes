4fbe887b4bf0af61acac560e16fa9c27f3277b71
Lucas Oliveira <lucassdeoliveira@outlook.com.br>
2026-05-23 00:49:05 -0300

    add 1-hello-world

---
+++ 1-hello-world/ola.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const espanhol = "espanhol"
+const frances = "frances"
+
+const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
+
+func Ola(nome string, idioma string) string {
+    if nome == "" {
+        nome = "Mundo"
+    }
+
+    return prefixodeSaudacao(idioma) + nome
+}
+
+func prefixodeSaudacao(idioma string) (prefixo string) {
+    switch idioma {
+    case frances:
+        prefixo = prefixoOlaFrances
+    case espanhol:
+        prefixo = prefixoOlaEspanhol
+    default:
+        prefixo = prefixoOlaPortugues
+    }
+    return
+}
+func main() {
+	fmt.Println(Ola("Mundo", ""));
+}
